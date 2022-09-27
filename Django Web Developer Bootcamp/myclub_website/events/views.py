149e5486b220be5b0bafbc67f8f8875620a933ae
zjambor <jambor.zoltan1@gmail.com>
2022-09-27 09:24:03 +0200

    ok

--- Django Web Developer Bootcamp/myclub_website/events/views.py
+++ Django Web Developer Bootcamp/myclub_website/events/views.py
@@ -1,6 +1,36 @@
+from time import strftime
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-# Create your views here.
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "John"
+    month = month.title()
 
-def home(request):
-    return render(request, 'home.html', {})
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # create calendar
+    cal = HTMLCalendar().formatmonth(
+        year,
+        month_number
+    )
+
+    # get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # get current time
+    time = now.strftime('%I:%M:%S %p')
+
+    return render(request,
+        'events/home.html', {
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time
+    })
