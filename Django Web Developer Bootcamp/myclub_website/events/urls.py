c82cc75a026021048d353cc10cb0da60ef2df13c
zjambor <jambor.zoltan1@gmail.com>
2022-09-30 09:44:08 +0200

    ok

--- Django Web Developer Bootcamp/myclub_website/events/urls.py
+++ Django Web Developer Bootcamp/myclub_website/events/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
 
     path('', views.home, name="home"),
     path('<int:year>/<str:month>/', views.home, name="home"),
-    path('events', views.all_events, name="list-events")
+    path('events', views.all_events, name="list-events"),
+    path('add_venue', views.add_venue, name="add-venue")
 ]
